574a921fe4ead86dc25673f4f49c39941ecc91c9
Ernesto Samaniego <esamaniego53@gmail.com>
2019-09-03 21:23:28 -0700

    added calculations

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,28 @@
 $(document).ready(function($) {
 	
     let calcBtn = $('#calc-btn');
-    let hgb = $('#');
-    let hct = $('#hct');
-    let rbc = $('#rbc');
     
+ 
     calcBtn.on('click', (e) => {
         e.preventDefault();
+
+        // set up variables
+        let hgb = $("#hgb").val();
+        let hct = $("#hct").val();
+        let rbc = $("#rbc").val();
+        // MCV Calculation here
+        let mcvCalculation = hct * 10 / rbc;
+        // MCHC calculation here
+        let mchcCalculation = hgb * 100 / hct;
+
+            // MCV calculation is  Hct *10 / Rbc;
+            // mchc calculation is Hgb *100 / Hct;
+
+       
+        $('#mcv').html('Calculated MCV: ' + mcvCalculation.toFixed(1));
+        $('#mchc').html('Calculated MCHC: ' + mchcCalculation.toFixed(1));
+        console.log('mcv : ' + mcvCalculation);
+        console.log('mchc : ' + mchcCalculation);
 
     });
 });
