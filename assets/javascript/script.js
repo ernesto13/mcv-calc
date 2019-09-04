19aec0cc0f88de66109d666692e24569fecfd7da
Ernesto Samaniego <esamaniego53@gmail.com>
2019-09-03 19:36:57 -0700

    added form and modal

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function($) {
 	
-	
+    let calcBtn = $('#calc-btn');
+    let hgb = $('#');
+    let hct = $('#hct');
+    let rbc = $('#rbc');
+    
+    calcBtn.on('click', (e) => {
+        e.preventDefault();
+
+    });
 });
